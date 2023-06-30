3423c07535960717115c6e92fcda9fb59ccb2f58
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2023-07-01 00:54:41 +0300

    updated the authentication

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -1,95 +1,171 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { auth, db } from "../config/firebase";
-import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, sendPasswordResetEmail, onAuthStateChanged} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, sendPasswordResetEmail} from "firebase/auth";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
-// import { useDispatch } from "react-redux";
 
-// const dispatch = useDispatch();
+// This is a thunk, that login user with email and password
+export const signinWithEmail =  createAsyncThunk(
+    "auth/signinWithEmail", 
+    async (payload) => {
+        const {email, password} = payload
+        await signInWithEmailAndPassword(auth, email, password);   
+    }
+)
+
+// This is a thunk, that login user with google
+export const signinWithGoogle = createAsyncThunk(
+    "auth/signinWithGoogle",
+    async () => {
+        await signInWithPopup(auth, new GoogleAuthProvider())
+    }
+)
+
+// This is a thunk, that signup user with email,password, and fullName 
+// Changes can be made to sign up by providing full details of the user
+export const signup =  createAsyncThunk(
+    "auth/signup", 
+    async (payload) => {
+        const {email, password, fullName} = payload
+        const newUser = await createUserWithEmailAndPassword(auth, email, password)
+        const userRef = doc(db, "users", newUser.user.uid)
+        await updateProfile(newUser.user, {displayName: fullName})
+        await setDoc(userRef, {
+            fullName: fullName,
+            email: email,
+            createdAt: serverTimestamp(),
+        })
+
+})
+
+// This is a thunk, that signout user
+export const signout = createAsyncThunk(
+    "auth/signout",
+    async () => {
+        await signOut(auth)
+    }
+)
+
+
+// This is a thunk, that reset password
+export const resetPassword = createAsyncThunk(
+    "auth/resetPassword",
+    async (payload) => {
+        const {email} = payload
+        await sendPasswordResetEmail(auth, email)
+    }
+)
+
+
 
 const authSlice = createSlice({
     name: 'auth',
-    
     initialState: {
-        currentUser: null
+        currentUser: null,
+        authStatus: 'idle',
+        authError: null,
     },
 
     reducers: {
-        signinWithEmail: async (state, action) => {
-            try{
-                
-                const {email, password} = action.payload
-                const userCred = await signInWithEmailAndPassword(auth, email, password);
-                console.log("+++++++++++++++++++++++++++++",userCred.user)
-                // state.currentUser = setCurrentUser(userCred.user);
-            }catch(error){
-                console.log(error)
-            }
-        },
-
-        signinWithGoogle: async (state) => {
-            try{
-                const userCred = await signInWithPopup(auth, new GoogleAuthProvider())
-                state.currentUser = userCred.user;
-            }catch(error){
-                console.log(error)
-            }
-        },
-
-
-        signup: async (state, action) => {
-            try{
-                const {email, password, fullName} = action.payload
-                const newUser = await createUserWithEmailAndPassword(auth, email, password)
-                const userRef = doc(db, "users", newUser.user.uid)
-                await updateProfile(newUser.user, {displayName: fullName})
-                await setDoc(userRef, {
-                    fullName: fullName,
-                    email: email,
-                    createdAt: serverTimestamp(),
-                });
-                // state.currentUser = setCurrentUser(newUser.user);
-            }catch(error){
-                console.log(error)
-            }
-
-        },
-        signout: async (state) => {
-            await signOut(auth);
-            state.currentUser = null;
-        },
-
-        resetPassword: async (state, action) => {
-            try{
-                const {email} = action.payload
-                await sendPasswordResetEmail(auth, email)
-            }catch(error){
-                console.log(error)
-            }
-        },
-
-
         setCurrentUser: (state, action) => {
-            if (!action.payload) {
-                state.currentUser = null;
-                return;
-            }
-            const {displayName, email, uid, photoURL} = action.payload;
-            state.currentUser = {displayName: displayName, email:email, uid: uid, photoURL:photoURL};
+            const user = action.payload;
+            state.currentUser = user ? user: null;
         }
         
+    },
+
+    extraReducers: (builder) =>{
+        builder
+            .addCase(signinWithEmail.pending, (state) => {
+                state.authStatus = 'loading';
+                state.authError = null;
+            })
+            .addCase(signinWithEmail.fulfilled, (state) => {
+                // state.currentUser = action.payload;
+                state.authStatus = 'succeeded';
+            })
+            .addCase(signinWithEmail.rejected, (state, action) => {
+                console.log(action.error.message)
+                state.authStatus = 'failed';
+                state.authError = action.error.message;
+            })
+
+
+
+
+            .addCase(signup.pending, (state) => {
+                state.authStatus = 'loading';
+                state.authError = null;
+            })
+            .addCase(signup.fulfilled, (state) => {
+                state.authStatus = 'succeeded';
+            })
+            .addCase(signup.rejected, (state, action) => {
+                state.authStatus = 'failed';
+                state.authError = action.error.message;
+            })
+            
+
+
+
+            .addCase(signinWithGoogle.pending, (state, action) => {
+                state.authStatus = 'loading';
+                state.authError = action.error.message;
+                })
+            .addCase(signinWithGoogle.fulfilled, (state) => {
+                state.authStatus = 'succeeded';
+            })
+            .addCase(signinWithGoogle.rejected, (state, action) => {
+                state.authStatus = 'failed';
+                state.authError = action.error.message;
+            })
+
+
+
+
+            .addCase(signout.pending, (state) => {
+                state.authStatus = 'loading';
+                state.authError = null;
+            })
+            .addCase(signout.fulfilled, (state) => {
+                state.authStatus = 'idle';
+            })
+            .addCase(signout.rejected, (state, action) => {
+                state.authStatus = 'failed';
+                state.authError = action.error.message;
+            })
+
+
+
+            .addCase(resetPassword.pending, (state) => {
+                state.authStatus = 'loading';
+                state.authError = null;
+            })
+            .addCase(resetPassword.fulfilled, (state) => {
+                state.authStatus = 'idle';
+            })
+            .addCase(resetPassword.rejected, (state, action) => {
+                state.authStatus = 'failed';
+                state.authError = action.error.message;
+            })
+
     }
 })
 
 
+
+
 export const listenForAuthChanges = () => (dispatch) => {
     auth.onAuthStateChanged((user) => {
-      if (user) {
+        user = user ? {
+            uid: user.uid,
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+        } : null;
         dispatch(setCurrentUser(user));
-      } else {
-        dispatch(setCurrentUser(null));
-      }
     });
   };
 
-export const {signinWithEmail, signinWithGoogle, signup, signout, setCurrentUser, resetPassword} = authSlice.actions;
+
+export const {setCurrentUser} = authSlice.actions;
 export default authSlice.reducer;
